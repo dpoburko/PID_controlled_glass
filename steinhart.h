c0fb29aad6c03e7a1a68ab3d4ebc2bca9c550482
PoburkoLab <42272290+dpoburko@users.noreply.github.com>
2024-04-16 07:00:18 -0700

    Update steinhart.h
    initial go

--- steinhart.h
+++ steinhart.h
@@ -7,20 +7,48 @@ class steinhart {
 
   public:
   //constants used in functions
-    //constructor, needs analog in PIN, nomimal thermistor resistance, nominal temp (C), B coeeficient, series resistor 
-    steinhart(int, long, long, long,long);
+  #define CELCIUS 1
+  #define FARENHEIT 0
+  
+
+    //constructor, needs analog in PIN, temperature out, nomimal thermistor resistance, nominal temp (C), B coeeficient, series resistor 
+    steinhart(int, int, float, long, long, long,long);
       
     bool Compute(); //performs calculation from current voltage input
 
-    bool outputScale(); // celcius or farenheit
+    int celciusOut(); // celcius or farenheit
     
     void setSampleTime(); //define time between measurements
+
+    void setAnalogSamples(); //number of times the analog in pin is averaged
       
   //Display functions:
     long getNominalResistance();
     long getCoefficient();
     long getNominalTemp();
     long getSeriesResistance();
+    
 
 }  
 
+  private:
+    void Initialize();
+    //pointers to the Steinhart parameters
+    int *analogPin;
+    int *analogIn;
+    float *steinhartOut;
+    long *thermistoResistance; 
+    long *thermistorNominalTemp;
+    long *bCoefficient;
+    long *seriesResistor;
+    inCelcius
+    
+    unsigned long prevTime;
+    unsgined long sampelTime;
+    bool inCelcius;
+    int nSamples;
+    float thermistorReading;
+    float thermistorVoltage;
+    
+}; //close steinhart class
+#endif
