e5f8b2888d7d17cd6a63102f8ad8b827df16c9e8
PoburkoLab <42272290+dpoburko@users.noreply.github.com>
2024-08-20 12:57:33 -0700

    Update heaterStatus.h

--- heaterStatus.h
+++ heaterStatus.h
@@ -1,1 +1,22 @@
+#ifndef heaterStatus_H
+#define heaterStatus_H
+
+#include "Arduino.h"
+
+#define LIBRARY_VERSION 1.0.0
+
+//place variables and functions names here
+
+class heaterStatus {
+heaterStatus
+
+
+  public:
+  //constants used in functions
+
+  private:
+
+
+}; //close heaterStatus class
+#endif
 
