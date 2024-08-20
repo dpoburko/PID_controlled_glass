327775191f2029f4b897ee90a42c02776f3bc82e
PoburkoLab <42272290+dpoburko@users.noreply.github.com>
2024-08-20 13:00:13 -0700

    Update heaterStatus.cpp

--- heaterStatus.cpp
+++ heaterStatus.cpp
@@ -1,1 +1,15 @@
+#include <Arduino.h>
+#include "heaterStatus.h"
 
+//checking if PID controlled glass heater is working as expected
+
+
+//Constructor
+// pointers look like type* nameToPointer
+heaterStatus::heaterStatus(type* nameToPointer) {
+
+  //make local version of variables that are received as pointers from .ino file
+  localVariable = nameToPointer;
+  
+}
+    
