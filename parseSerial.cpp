214701c21517164b414f812ce6e90ce100701837
PoburkoLab <42272290+dpoburko@users.noreply.github.com>
2025-09-26 12:23:27 -0700

    confirmed working
    serial display timer change by serial monitor works
    min interval = 2s

--- parseSerial.cpp
+++ parseSerial.cpp
@@ -251,6 +251,9 @@
 
       int seconds = atoi(tmr);                // Convert to int
       *serialDisplayInterval = seconds * 1000; // Convert to milliseconds
+      if (*serialDisplayInterval <  2000){
+        *serialDisplayInterval =  2000;
+      }
           
     } 
     else if(serialMain->incoming[0] == 'E' && serialMain->incoming[1] == 'a' )
