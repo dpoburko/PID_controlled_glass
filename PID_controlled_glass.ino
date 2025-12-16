f2e9523733d172211a4ee3eee9efad68fb4563d4
PoburkoLab <42272290+dpoburko@users.noreply.github.com>
2025-12-16 10:12:06 -0800

    erroneous reading filter
    testing comparing and replacing erroneous value with average of previous 3 readings

--- PID_controlled_glass.ino
+++ PID_controlled_glass.ino
@@ -40,7 +40,7 @@
   generalSensor enclosureTemperature(35, "enclosure temperature", 22.0, 37.0, 20, 1000, 40.0, 20.0);
 
 //PIDextra(double aP, double aI, double aD, double aSetpoint, double amaxOutputNormal,double amaxOutputHigh, double errorOutput int aMode)
-  PIDextras heaterValues(2.0, 96.0, 600.0, lidTemperature.setpoint, 50.0,60.0,10.0,1);
+  PIDextras heaterValues(2.0, 96.0, 200.0, lidTemperature.setpoint, 50.0,60.0,10.0,1);
 
 String errorBuffer;
 String msgBuffer;
@@ -197,7 +197,7 @@ void loop()
 
     // Check to make sure the temperature data points makes sense, and reassign the value if necessary
     RemoveErroneousSensorReadings(lidTemperature,0.6,msgBuffer);
-    RemoveErroneousSensorReadings(enclosureTemperature,0.1,msgBuffer);
+    RemoveErroneousSensorReadings(enclosureTemperature,0.08,msgBuffer);
 
     // Log the temperatures and calculate slopes and MSE
     sensorUpdate(lidTemperature);
--- functions.h
+++ functions.h
@@ -126,19 +126,30 @@ void RemoveErroneousSensorReadings(generalSensor &sensor, double tolerance, Stri
   double prev1Value = sensor.history[(sensor.index - 1 + sensor.historySize)%sensor.historySize]; 
   double prev2Value = sensor.history[(sensor.index - 2 + sensor.historySize)%sensor.historySize]; 
   double prev3Value = sensor.history[(sensor.index - 3 + sensor.historySize)%sensor.historySize]; 
-  //double currSlope = sensor.slope[(sensor.index - 1 + sensor.historySize)%sensor.historySize]; 
-  //if (currSlope >0.1) tolerance = 2.0 * tolerance;
+  double prev4Value = sensor.history[(sensor.index - 4 + sensor.historySize)%sensor.historySize]; 
+  double currSlope = sensor.slope[(sensor.index - 1 + sensor.historySize)%sensor.historySize]; 
+  if (currSlope >0.1) tolerance = 2.0 * tolerance;
+
+  double prev3Average = (prev1Value + prev2Value + prev3Value)/3; 
+
   
-  if ( (sensor.historyFilled == true) && (prev1Value != prev2Value) )
+  if ( (sensor.historyFilled == true) && (prev1Value != prev2Value) && (prev1Value != prev3Value) )
   {
-    if ( (abs(sensor.value - prev1Value) > tolerance) && (abs(sensor.value - prev2Value) > tolerance) ) 
+//    if ( (abs(sensor.value - prev1Value) > tolerance) && (abs(sensor.value - prev2Value) > tolerance) ) 
+//    {
+//      replaceValue = true;
+//    }
+
+    if  (abs(sensor.value - prev3Average) > tolerance)
     {
       replaceValue = true;
     }
+
   } else {
 
     //for the enclosure thermistor, sometimes two consecutive readings will end up being equal.  
-    if ( (abs(sensor.value - prev1Value) > tolerance*2.0) && (abs(sensor.value - prev2Value) > tolerance*2.0) ) 
+    //if ( (abs(sensor.value - prev3Average) > tolerance*2.0) && (abs(sensor.value - prev2Value) > tolerance*2.0) ) 
+    if ( abs(sensor.value - prev3Average) > tolerance*2.0) 
     {
       replaceValue = true;
     }
@@ -146,7 +157,7 @@ void RemoveErroneousSensorReadings(generalSensor &sensor, double tolerance, Stri
   }
 
   //need a catch to ensure thath the system doesn't lock into 1 value. 
-  if ( (prev1Value == prev3Value) && (prev2Value == prev3Value) ) {
+  if ( (prev1Value == prev2Value) && (prev1Value == prev3Value) && (prev1Value == prev4Value) ) {
     replaceValue = false;
   }
 
@@ -161,7 +172,8 @@ void RemoveErroneousSensorReadings(generalSensor &sensor, double tolerance, Stri
       msgBuffer += " & ";
       msgBuffer += String(prev2Value,3);
       msgBuffer += ". Reassigned to ";
-      sensor.value = (prev1Value + prev2Value)/2;
+      //sensor.value = (prev1Value + prev2Value)/2;
+      sensor.value = prev3Average;
       sensor.history[(sensor.index - 1 + sensor.historySize)%sensor.historySize] = sensor.value; 
       msgBuffer += sensor.value;
     
